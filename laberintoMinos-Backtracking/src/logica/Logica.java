36966cb39a8e2e5f2758f3aec802771eb7c4ac2a
EstebanOG <juanesteban199911@gmail.com>
2021-05-02 18:40:08 -0500

    Se agregan notificaciones

--- laberintoMinos-Backtracking/src/logica/Logica.java
+++ laberintoMinos-Backtracking/src/logica/Logica.java
@@ -1,6 +1,7 @@
 package logica;
 
 import java.util.ArrayList;
+import javax.swing.JOptionPane;
 import vista.Vista;
 
 /**
@@ -16,7 +17,8 @@ public class Logica{
      * 4: Jovenes
      */
     private Vista vista = new Vista();
-    private boolean minoVivo = true;
+    private int numPasos = 0,numPasosCambio = 13, numJovenes = 0, numJovenesEncontrados = 0;
+    private boolean minoVivo = true, juego = true;
     private ArrayList<String> camino = new ArrayList<String>();
     private int tablero[][] = {{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
                                {3,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1},
@@ -40,13 +42,17 @@ public class Logica{
                                 };
     public Logica() {
         
-        //generarJovenes();
+        generarJovenes();
         vista.getMapa().repintarMapa(tablero);
-        while(minoVivo){
+        while(juego){
+            if(numJovenesEncontrados>0){
+                juego = false;
+            }
             camino.clear();
             recorrer(1,0,tablero,"");
+            
         }
-        
+        notificar(5);
         
     }
 
@@ -58,9 +64,15 @@ public class Logica{
         this.tablero = tablero;
     }
     
+    /**
+     * Generar los jovenes que se encuentran al interior del laberinto.
+     */
     private void generarJovenes(){
-        int numJovenes = (int) Math.floor(Math.random()*3+1);
+        //Generar número de jovenes, el cual está entre 1 y 3
+        numJovenes = (int) Math.floor(Math.random()*3+1);
         int contador = 0;
+        
+        //Se buscan casillas alteatorias disponibles para ubicar a los jovenes.
         while(contador<numJovenes){
             int x = (int) Math.floor(Math.random()*18);
             int y = (int) Math.floor(Math.random()*20);
@@ -75,102 +87,213 @@ public class Logica{
     
     public void recorrer(int x, int y, int[][] tableroAct, String pasoAnterior) {
         //Movimiento derecha
-        try {
-            Thread.sleep(100);
-        } catch (Exception e) {
-            System.out.println(e);
-        }
+        pausar();
         vista.getMapa().repintarMapa(tableroAct);
-
-        if (minoVivo && tableroAct[x - 1][y] != 1 && !camino.contains("" + x + "" + y + "" + (x - 1) + "" + y)) {
+        
+        if (x>0 && juego && tableroAct[x - 1][y] != 1 && !camino.contains("" + x + "" + y + "" + (x - 1) + "" + y)) {
             if (tableroAct[x - 1][y] == 2) {
+                notificar(0);
                 minoVivo = false;
+                tableroAct[x - 1][y] = 3;
             }
+            if(tableroAct[x - 1][y] == 4){
+                if(!minoVivo){
+                    notificar(3);
+                    numJovenesEncontrados++;
+                }
+            }
+            
             tableroAct[x][y] = 0;
-            tableroAct[x - 1][y] = 3;
+            
+            contarPasos();
             tablero = tableroAct;
-            System.out.println("Arriba");
             camino.add("" + x + "" + y + "" + (x - 1) + "" + y);
             recorrer((x - 1), y, tableroAct, "arriba");
             tableroAct[x][y] = 3;
             tableroAct[x - 1][y] = 0;
+            contarPasos();
         }
         vista.getMapa().repintarMapa(tableroAct);
-        try {
-            
-            Thread.sleep(100);
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-        if ( minoVivo &&  tableroAct[x][y + 1] != 1 && !camino.contains("" + x + "" + y + "" + x + "" + (y + 1))) {
+        pausar();
+        
+        if ( juego &&  tableroAct[x][y + 1] != 1 && !camino.contains("" + x + "" + y + "" + x + "" + (y + 1))) {
             if (tableroAct[x][y + 1] == 2) {
+                notificar(0);
                 minoVivo = false;
             }
+            
+            if(tableroAct[x][y + 1] == 4){
+                if(!minoVivo){
+                    notificar(3);
+                    numJovenesEncontrados++;
+                }
+            }
+            
+            
             tableroAct[x][y] = 0;
             tableroAct[x][y + 1] = 3;
+            contarPasos();
             tablero = tableroAct;
-            System.out.println("Derecha");
             camino.add("" + x + "" + y + "" + x + "" + (y + 1));
             recorrer(x, (y + 1), tableroAct, "derecha");
             tableroAct[x][y] = 3;
             tableroAct[x][y + 1] = 0;
+            contarPasos();
         }
         vista.getMapa().repintarMapa(tableroAct);
-        try {
-            //Ponemos a "Dormir" el programa durante los ms que queremos
-            Thread.sleep(100);
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-        if ( minoVivo &&  tableroAct[x + 1][y] != 1 && !camino.contains("" + x + "" + y + "" + (x + 1) + "" + y)) {
+        pausar();
+        
+        if ( juego &&  tableroAct[x + 1][y] != 1 && !camino.contains("" + x + "" + y + "" + (x + 1) + "" + y)) {
             if (tableroAct[x + 1][y] == 2) {
+                notificar(0);
                 minoVivo = false;
+            }
+            
+            if(tableroAct[x + 1][y] == 4){
+                if(!minoVivo){
+                    notificar(3);
+                    numJovenesEncontrados++;
+                }
             }
             tableroAct[x][y] = 0;
             tableroAct[x + 1][y] = 3;
+            contarPasos();
             tablero = tableroAct;
-            System.out.println("Abajo");
             camino.add("" + x + "" + y + "" + (x + 1) + "" + y);
             recorrer((x + 1), y, tableroAct, "abajo");
             tableroAct[x][y] = 3;
             tableroAct[x + 1][y] = 0;
+            contarPasos();
         }
         vista.getMapa().repintarMapa(tableroAct);
-        try {
-            //Ponemos a "Dormir" el programa durante los ms que queremos
-            Thread.sleep(100);
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-        if (y > 0 && minoVivo &&  tableroAct[x][y - 1] != 1 && !camino.contains("" + x + "" + y + "" + x + "" + (y - 1))) {
+        pausar();
+        
+        if (y > 0 && juego &&  tableroAct[x][y - 1] != 1 && !camino.contains("" + x + "" + y + "" + x + "" + (y - 1))) {
             if (tableroAct[x][y - 1] == 2) {
+                notificar(0);
                 minoVivo = false;
+            }
+            
+            if(tableroAct[x][y - 1] == 4){
+                if(!minoVivo){
+                    notificar(3);
+                    numJovenesEncontrados++;
+                }
             }
             tableroAct[x][y] = 0;
             tableroAct[x][y - 1] = 3;
+            contarPasos();
             tablero = tableroAct;
-            System.out.println("Izquierda");
             camino.add("" + x + "" + y + "" + x + "" + (y - 1));
             recorrer(x, (y - 1), tableroAct, "izquierda");
             tableroAct[x][y] = 3;
             tableroAct[x][y - 1] = 0;
+            contarPasos();
         }
         
         vista.getMapa().repintarMapa(tableroAct);
+        pausar();
+    }
+    
+    
+    /**
+     * Cambiar la posición del minotauro.
+     */
+    private void cambiarMinotauro(){
+        boolean cambiarMino = true;
+        
+        int posXMino = 0,
+            posYMino = 0;
+       
+        //Buscar Minotaruo
+        for(int i = 0; i < tablero.length; i++){
+            for(int j = 0; j < tablero[0].length; j++){
+                if(tablero[i][j]==2){
+                    posXMino = i;
+                    posYMino = j;
+                }
+            }
+        }
+        
+        //Se repite hasta cambiar el minotauto.
+        while(cambiarMino){
+            //Se genera una casilla aleatoria.
+            int x = (int) Math.floor(Math.random()*18);
+            int y = (int) Math.floor(Math.random()*20);
+            
+            //Se verfica si la casilla está disponible, se encuentra teseo
+            //o alguno de los jovenes.
+            if(tablero[x][y] ==  0 ){
+                tablero[posXMino][posYMino] = 0;
+                tablero[x][y] = 2;
+                cambiarMino = false;
+            }
+            if(tablero[x][y] == 3){
+                tablero[posXMino][posYMino] = 0;
+                tablero[x][y] = 2;
+                notificar(1);
+                juego = false;
+                cambiarMino = false;
+            }
+            
+            if(tablero[x][y]==4){
+                numJovenes--;
+                tablero[posXMino][posYMino] = 0;
+                tablero[x][y] = 2;
+                if(numJovenes == 0){
+                  juego =false;
+                  notificar(4);  
+                }else{
+                    notificar(2); 
+                }
+                cambiarMino = false;
+            }
+        }
+    }
+    
+    private void pausar(){
         try {
-            Thread.sleep(100);
+            Thread.sleep(10);
         } catch (Exception e) {
             System.out.println(e);
         }
     }
     
-    private void mostrarLaberinto(){
-        for(int i = 0; i < tablero.length; i++){
-            for(int j = 0; j < tablero[0].length; j++){
-                System.out.print(tablero[i][j]+"-");
-            }
-            System.out.println("");
+    private void notificar(int op){
+        switch(op){
+            case 0:
+                JOptionPane.showMessageDialog(null, "Minotauro eliminado.");
+                break;
+            case 1:
+                JOptionPane.showMessageDialog(null, "Teseo eliminado. El juego a terminado.");
+                System.exit(0);
+                break;
+            case 2:
+                JOptionPane.showMessageDialog(null, "Joven eliminado.");
+                break;
+            case 3:
+                JOptionPane.showMessageDialog(null, "Joven encontrado.");
+                break;
+            case 4:
+                JOptionPane.showMessageDialog(null, "Los jovenes han sido eliminados. El juego ha termiando.");
+                System.exit(0);
+                break;
+            case 5:
+                JOptionPane.showMessageDialog(null, "Juego terminado.");
+                break;
+            default:
+                break;
         }
+        
     }
     
+    private void contarPasos(){
+        numPasos++;
+        System.out.println("Pasos:"+numPasos);
+        numPasosCambio--;
+        if(minoVivo && numPasosCambio == 0){
+            cambiarMinotauro();
+            numPasosCambio = 13;
+        }
+    }
 }
